2dbb20333226b03f1bdc84554ae4ed7e2a07285d
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-21 15:05:13 -0700

    display all pie options in a listview on PiesActivity

--- app/src/main/java/com/example/guest/laurettajean/PiesActivity.java
+++ app/src/main/java/com/example/guest/laurettajean/PiesActivity.java
@@ -2,12 +2,23 @@ package com.example.guest.laurettajean;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class PiesActivity extends AppCompatActivity {
+    private String[] pies = new String[] {"Apple Walnut", "Blackberry Raspberry Stresuel", "Tart Cherry", "Rhubarb Streusel", "Rhubarb Raspberry Sour Cream", "Blueberry Rhubarb", "Strawberry Rhubarb", "Chocolate Cream", "Coconut Cream", "Chocolate Banana Cream", "Banana Cream", "Blackbottom Coconut", "Key Lime", "Lemon Gingersnap", "Salted Honey", "Honey Hazelnut", "Chocolate Chess", "S'mores"};
+    @Bind(R.id.pieListView) ListView mPieListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pies);
+        ButterKnife.bind(this);
+
+        ArrayAdapter pieAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, pies);
+        mPieListView.setAdapter(pieAdapter);
     }
 }
