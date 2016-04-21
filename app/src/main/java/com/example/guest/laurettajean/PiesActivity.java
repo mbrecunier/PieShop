ee52ed85b1f34250605e23731d62167088418517
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-21 11:45:50 -0700

    create PiesActivity with header

---
+++ app/src/main/java/com/example/guest/laurettajean/PiesActivity.java
@@ -0,0 +1,13 @@
+package com.example.guest.laurettajean;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pies);
+    }
+}
